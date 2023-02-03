2aedc453b4497f5544991814529315153a8d37a2
Rohit Rai <66778858+GabbarVirus@users.noreply.github.com>
2023-02-03 15:06:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your code here
+const secondsHand = document.querySelector(".second-hand")
+const minsHand = document.querySelector(".min-hand")
+const hourHand = document.querySelector(".hour-hand")
+
+function setDate()
+{
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const secondsDegrees = 6 * seconds;
+    secondsHand.style.transform = `rotate(${secondsDegrees + 90}deg)`;
+
+    const mins = now.getMinutes();
+    const minsDegrees = 6 * mins;
+    minsHand.style.transform = `rotate(${minsDegrees + 90}deg)`;
+
+    const hour = now.getHours();
+    const hourDegrees = (30 * hour + mins / 2) % 360;
+    hourHand.style.transform = `rotate(${hourDegrees + 90}deg)`;
+}
+
+setInterval(setDate, 1000)
+
+setDate();
